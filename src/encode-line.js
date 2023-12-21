d2489988010d0b015b62a25480104352147a5d4b
Taras Zarytskyi <taras.zarytskyi@gmail.com>
2023-12-21 13:52:16 +0200

    feat: add encode-line task Th Dec 21 2023 13:51:01 pm

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  // if (typeof str !== 'string') {
+  //   throw new Error('Input must be a string');
+  // }
+
+  let resultStr = '';
+  let count = 1; // лічильник підрахунку послідовних входжень одного і того ж сиволу
+  let currentChar = str[0];
+
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] === str[i + 1]) {
+      count += 1; // збільшення лічильника якщо поточний симол рівний наступному
+    } else {
+      resultStr += (count > 1 ? count : '') + str[i]; // Додавання лічильника і символу до результату, з урахуванням випадку, коли лічильник більше 1.
+      count = 1; // скидання лічильника для наступного символу
+    }
+  }
+
+  return resultStr;
 }
 
 module.exports = {
