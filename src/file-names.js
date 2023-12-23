9346a32ef75c26858ce0ec1809f898eb4a731780
Taras Zarytskyi <taras.zarytskyi@gmail.com>
2023-12-23 06:39:38 +0200

    feat: add file-name task Fr Dec 23 2023 06:39:01 am

--- src/file-names.js
+++ src/file-names.js
@@ -15,9 +15,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+
+  let counter = 0 // ініт лічильника для відстеження номера суффіксу
+
+  // Зовнішній цикл для перебору імен файлів
+  for (let i = 0; i <= names.length; i += 1) {
+    // внутрішній цикл для порівнянн кожного імені файлу з іншим
+    for (let k = 0; k <= names.length; k += 1) {
+      // перевірка чи імена відрізняються та чи імена на позиціях i та k однакові
+      if (k !== i && names[k] === names[i]) {
+        counter = counter + 1;
+        // додавання лічильника як суфікса до повторюваного імені
+        names[k] = `${names[k]}(${counter})`;
+      }
+    }
+    counter = 0; // скидання лічильника для наступної ітераціїї
+  }
+
+  return names
 }
 
 module.exports = {
